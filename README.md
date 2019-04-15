7728e8882a89cc43a7752f1573ff7a9dbd2b95c4
chenyd <chenyd@billbear.cn>
2019-04-15 18:22:26 +0800

    add lesson1

---
+++ README.md
@@ -0,0 +1,6 @@
+
+#### Lesson1
+- scrapy startproject crawlproject
+- scrapy genspider -t basic toscrape-xpath quotes.toscrape.com
+- scrapy crawl toscrape-xpath -o toscrape-xpath.json
+
--- crawlproject/spiders/toscrape_xpath.py
+++ crawlproject/spiders/toscrape_xpath.py
@@ -8,4 +8,9 @@ class ToscrapeXpathSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield dict(
+                quote=quote.xpath('./span[@class="text"]/text()').extract_first(),
+                author=quote.xpath('.//small[@class="author"]/text()').extract_first(),
+                tags=quote.xpath('./div[@class="tags"]/a[@class="tag"]/text()').extract(),
+            )
