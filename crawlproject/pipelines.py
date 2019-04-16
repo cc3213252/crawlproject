a1e1a2ad132cc0588c83635809045470e238857f
chenyd <chenyd@billbear.cn>
2019-04-16 18:53:08 +0800

    add lesson5

--- crawlproject/pipelines.py
+++ crawlproject/pipelines.py
@@ -5,7 +5,19 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+import codecs
 
-class ToscrapeXpathPipeline(object):
+
+class JsonWriterPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = codecs.open('toscrape.js', 'wb', encoding='utf-8')
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item), ensure_ascii=False, sort_keys=False) + "\n"
+        self.file.write(line)
         return item
+
+    def close_spider(self, spider):
+        self.file.close()
