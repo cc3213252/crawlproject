48ab550153110368cee1f1a2ec9ef43d29789ef6
yudan.chen <cc3213252@163.com>
2019-04-21 20:54:28 +0800

    add lesson8

--- crawlproject/spiders/zol.py
+++ crawlproject/spiders/zol.py
@@ -14,5 +14,19 @@ class ZolSpider(scrapy.Spider):
             album_url = photo.xpath('./a/@href').get()
             title = photo.xpath('./a/@title').get()
             album_full_url = response.urljoin(album_url)
-            self.logger.info(title)
-            self.logger.info(album_full_url)
+            yield scrapy.Request(album_full_url, meta={'title': title}, callback=self.parse_album, dont_filter=True)
+
+    def parse_album(self, response):
+        title = response.meta['title']
+        img_url = response.xpath('//img[@id="bigImg"]/@src').get()
+        self.img_urls.append(img_url)
+        other_pics = response.xpath('//ul[@id="showImg"]/li')
+        for item in other_pics:
+            url = item.xpath('.//a/@href').get()
+            pic_full_url = response.urljoin(url)
+            yield scrapy.Request(pic_full_url, callback=self.parse_other_pic, dont_filter=True)
+
+    def parse_other_pic(self, response):
+        img_url = response.xpath('//img[@id="bigImg"]/@src').get()
+        self.img_urls.append(img_url)
+        self.logger.info(img_url)
